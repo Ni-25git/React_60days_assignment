3304c4a65b40c1d9479aec517a742498cbe62961
Ni-25git <nipunsherawat@gmail.com>
2024-05-18 09:09:40 +0530

    practise

---
+++ practise.html/index.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector("#btn");
+let a = document.querySelector("#name")
+
+const handleBtn=()=>{
+    a.innerText="Nipun Sehrawat"
+}
+
+btn.addEventListener("click",handleBtn)
