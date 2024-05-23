114e60ce4b2fa5f5fd100454d434387ee4a0782d
Ni-25git <nipunsherawat@gmail.com>
2024-05-23 15:17:53 +0530

    Added assignment 2

---
+++ React_Assignment_2_2/src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post({ name, year }) {
+  return (
+    <div>
+      <p>Name : {name}</p>
+      <p>Year : {year}</p>
+    </div>
+  );
+}
+
+export default Post;
---
+++ React_assignment_2_1/src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post({ name, year }) {
+  return (
+    <div>
+      <p>Title : {name}</p>
+      <p>Year : {year}</p>
+    </div>
+  );
+}
+
+export default Post;
